717381dd0400a23e622cb7a2a66edfee8ac11d1c
Wallace Maia <wallacemaia2007@gmail.com>
2025-10-23 16:47:35 -0300

    Feat: Create galeria html

--- src/app/galeria/galeria-component/galeria-component.ts
+++ src/app/galeria/galeria-component/galeria-component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LugarService } from '../../lugares/service/lugar-service';
+import { CategoriaService } from '../../categorias/categoria/service/categoria-service';
+import { Categoria } from '../../categorias/categoria/categoria-component';
+import { LugarClass } from '../../lugares/lugarClass';
+import { CategoriaClass } from '../../categorias/categoriaClass';
 
 @Component({
   selector: 'app-galeria-component',
   standalone: false,
-  templateUrl: './galeria-component.html',
-  styleUrl: './galeria-component.scss'
+  templateUrl: './galeria.html',
+  styleUrl: './galeria.scss'
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit{
+
+  lugares: LugarClass[] = [];
+  categorias: CategoriaClass[] = [];
+
+  constructor(
+    private lugaresService: LugarService,
+    private categoriasService: CategoriaService,
+  ){}
+
+  ngOnInit(): void {
+    this.categoriasService.listarTodos().subscribe(categorias => this.categorias = categorias)
+    this.lugaresService.listarTodos().subscribe(lugares => this.lugares = lugares)
+      
+  }
+
+  getTotalEstrelas(lugar: LugarClass): string{
+    return '&#9733'.repeat(lugar.avaliacao || 0) + '&#9734'.repeat(5 - (lugar.avaliacao || 0));
+  }
 
 }

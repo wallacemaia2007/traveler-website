9b7423654a842dde82ae8a38ae29b18007dab574
Wallace Maia <wallacemaia2007@gmail.com>
2025-10-26 23:00:07 -0300

    Feat: Implementing galeria filter

--- src/app/galeria/galeria-component/galeria-component.ts
+++ src/app/galeria/galeria-component/galeria-component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LugarService } from '../../lugares/service/lugar-service';
 import { CategoriaService } from '../../categorias/categoria/service/categoria-service';
-import { Categoria } from '../../categorias/categoria/categoria-component';
 import { LugarClass } from '../../lugares/lugarClass';
 import { CategoriaClass } from '../../categorias/categoriaClass';
 
@@ -9,26 +8,49 @@ import { CategoriaClass } from '../../categorias/categoriaClass';
   selector: 'app-galeria-component',
   standalone: false,
   templateUrl: './galeria.html',
-  styleUrl: './galeria.scss'
+  styleUrl: './galeria.scss',
 })
-export class GaleriaComponent implements OnInit{
-
+export class GaleriaComponent implements OnInit {
   lugares: LugarClass[] = [];
+  lugaresFiltrados: LugarClass[] = [];
   categorias: CategoriaClass[] = [];
 
-  constructor(
-    private lugaresService: LugarService,
-    private categoriasService: CategoriaService,
-  ){}
+  filtroNome: string = '';
+  filtroCategoria: string = '';
+
+  constructor(private lugaresService: LugarService, private categoriasService: CategoriaService) {}
 
   ngOnInit(): void {
-    this.categoriasService.listarTodos().subscribe(categorias => this.categorias = categorias)
-    this.lugaresService.listarTodos().subscribe(lugares => this.lugares = lugares)
-      
+    this.categoriasService.listarTodos().subscribe((categorias) => {
+      this.categorias = categorias;
+    });
+
+    this.lugaresService.listarTodos().subscribe((lugares) => {
+      this.lugares = lugares;
+      this.lugaresFiltrados = lugares;
+    });
   }
 
-  getTotalEstrelas(lugar: LugarClass): string{
-    return '&#9733'.repeat(lugar.avaliacao || 0) + '&#9734'.repeat(5 - (lugar.avaliacao || 0));
+  aplicarFiltros(): void {
+    this.lugaresFiltrados = this.lugares.filter((lugar) => {
+      const correspondeNome =
+        !this.filtroNome || lugar.nome?.toLowerCase().includes(this.filtroNome.toLowerCase());
+
+      const correspondeCategoria =
+        !this.filtroCategoria || lugar.categoria === this.filtroCategoria;
+
+      return correspondeNome && correspondeCategoria;
+    });
   }
 
+  limparFiltros(): void {
+    this.filtroNome = '';
+    this.filtroCategoria = '';
+    this.lugaresFiltrados = this.lugares;
+  }
+
+  getTotalEstrelas(lugar: LugarClass): string {
+    const avaliacao = lugar.avaliacao || 0;
+    return '&#9733;'.repeat(avaliacao) + '&#9734;'.repeat(5 - avaliacao);
+  }
 }
--- src/app/galeria/galeria-module.ts
+++ src/app/galeria/galeria-module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule para ngModel
 
 import { GaleriaRoutingModule } from './galeria-routing-module';
 import { GaleriaComponent } from './galeria-component/galeria-component';
 
-
 @NgModule({
-  declarations: [
-    GaleriaComponent
-  ],
-  imports: [
-    CommonModule,
-    GaleriaRoutingModule
-  ]
+  declarations: [GaleriaComponent],
+  imports: [CommonModule, GaleriaRoutingModule, FormsModule],
 })
-export class GaleriaModule { }
+export class GaleriaModule {}

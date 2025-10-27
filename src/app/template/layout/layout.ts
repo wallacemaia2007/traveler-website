16acbe795b9e18f1022d5abc99b34894deb450cb
Wallace Maia <wallacemaia2007@gmail.com>
2025-10-26 23:24:54 -0300

    Feat: Add links routes for header toolbar

--- src/app/template/layout/layout.ts
+++ src/app/template/layout/layout.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutProps } from './layout-props';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -6,6 +9,28 @@ import { Component } from '@angular/core';
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
-export class Layout {
+export class Layout implements OnInit{
+
+  props: LayoutProps = { titulo: '', subTitulo: ''}
+
+  constructor( private router: Router, private activatedRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+
+    this.router.events.pipe(
+      filter( () => this.activatedRoute.firstChild !== null), map( () => this.obterPropriedadesLayout())
+    ).subscribe( (props: LayoutProps) => this.props = props )
+
+      
+  }
+
+  obterPropriedadesLayout(): LayoutProps{
+    let rotaChildren = this.activatedRoute.firstChild;
+
+    while(rotaChildren?.firstChild){
+      rotaChildren = rotaChildren.firstChild;
+    }
+    return rotaChildren?.snapshot.data as LayoutProps;
+  }
 
 }
--- src/app/template/template-routing-module.ts
+++ src/app/template/template-routing-module.ts
@@ -12,16 +12,19 @@ const routes: Routes = [
         loadChildren: () =>
           import('../categorias/categorias-module').then((m) => m.CategoriasModule),
         pathMatch: 'full',
+        data: { titulo: 'Categorias', subTitulo: 'Realize o cadastro de novas categorias!' }
       },
       {
         path: 'lugares',
         loadChildren: () => import('../lugares/lugares-module').then((m) => m.LugaresModule),
         pathMatch: 'full',
+        data: { titulo: 'Lugares', subTitulo: 'Realize o cadastro de novos lugares!' }
       },
       {
         path: 'galeria',
         loadChildren: () => import('../galeria/galeria-module').then((m) => m.GaleriaModule),
         pathMatch: 'full',
+        data: { titulo: 'Galeria', subTitulo: 'Descubra os melhores lugares para viajar e passear!' }
       },
     ],
   },
